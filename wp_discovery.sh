8186630a7da0421092917fc350b27f7207a52708
Sterling Anderson <sanderson1@andermacbookair.local>
2025-04-21 09:49:12 -0500

    Updates for my environment.

--- wp_discovery.sh
+++ wp_discovery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-WP_PATHS=($(find /var/www/*/web -type f -name "wp-config.php" -exec dirname {} \;))
+WP_PATHS=($(find /var/www/* -maxdepth 2 -type f -name "wp-config.php" -exec dirname {} \;))
 
 # JSON for Zabbix LLD 
 FIRST=true
@@ -7,13 +7,12 @@ echo -n '{"data":['
 
 for FULL_PATH in "${WP_PATHS[@]}"; do
     # extract the wp-folder name
-    INSTALL_NAME=$(echo "$FULL_PATH" | sed -E 's|/var/www/([^/]+)/web|\1|')
     if [ "$FIRST" = true ]; then
         FIRST=false
     else
         echo -n ','
     fi
-    echo -n "{\"{#WP_PATH}\":\"$INSTALL_NAME\"}"
+    echo -n "{\"{#WP_PATH}\":\"$FULL_PATH\"}"
 done
 
 echo -n ']}'
